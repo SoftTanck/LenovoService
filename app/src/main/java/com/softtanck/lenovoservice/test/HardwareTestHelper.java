e381bc1a2c81cfa0e04ae95ff1b981d0f60c1d44
tangce <i0555@cienet.com.cn>
2015-08-10 11:00:28 +0800

    update base.

---
+++ app/src/main/java/com/softtanck/lenovoservice/test/HardwareTestHelper.java
@@ -0,0 +1,24 @@
+package com.softtanck.lenovoservice.test;
+
+/**
+ * @author : Tanck
+ * @Description : TODO
+ * @date 8/10/2015
+ */
+public abstract class HardwareTestHelper {
+
+
+    public static enum TestResult {
+        TEST_FAIL,//检测失败
+        TEST_INTERRUPT,//检测中断
+        TEST_NOT_OK,//有问题
+        TEST_OK// 正常
+    }
+
+    /**
+     * 开始测试 <b>注意:在start前需要手动调用该方法</b>
+     *
+     * @return
+     */
+    public abstract TestResult start();
+}
